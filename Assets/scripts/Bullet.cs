b918bd7c032e9ad3e0cd2a5c8c086e84f56e1683
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-27 14:40:25 +0100

    Create Bullet class

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private Rigidbody2D rb;
+
+	private float lifeTime = 0.4f;
+	private float speed = 20;
+	private float timer;
+
+	private Vector2 direction;
+	public  Vector2 Direction {
+		set {
+			direction = value;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = direction * speed;
+		Destroy (gameObject, lifeTime);
+	}
+
+	private void OnTriggerEnter2D (Collider2D trig) {
+		if (trig.tag == "enemy") {
+			Destroy (gameObject);
+			Destroy (trig.gameObject);
+		}
 	}
 }
